0b58f71c3638a138a9abf8b83a761056c76ff593
Cameron Gorrie <sastraxi@gmail.com>
2020-03-13 13:43:19 -0400

    use env var as given by mongolab

--- src/util/mongo-client.ts
+++ src/util/mongo-client.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 // generate database URL from environment
-export const databaseUrl = process.env.MONGO_URI ||
+export const databaseUrl = process.env.MONGODB_URI ||
   `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}` +
     `@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`;
 
